6a6cffec735ad807093e7824d75e9ad53878c6a3
Aaron Archundia <archundiabaa@gmail.com>
2024-07-24 18:43:05 +0000

    /* Se utiliza para mantener los archivos CSS,JS,estaticos.
    express.static(__dirname + /path)
    
    Donde public será la carpeta donde se encuentre en este caso la hoja de estilos.
    */

--- myApp.js
+++ myApp.js
@@ -1,8 +1,17 @@
 let express = require('express');
 let app = express();
 
-app.get
-("/",
+/*
+Se utiliza para mantener los archivos CSS,JS,estaticos.
+
+express.static(__dirname + /path)
+
+Donde public será la carpeta donde se encuentre en este caso la hoja de estilos. 
+*/
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get("/",
     function(req, res)
     {
         var absolutePath = __dirname + '/views/index.html';
