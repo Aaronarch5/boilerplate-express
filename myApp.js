3756ce8215c76c19d28bb54f45bccfe49fcab33e
Aaron Archundia <archundiabaa@gmail.com>
2024-07-30 01:00:53 +0000

    .env use

--- myApp.js
+++ myApp.js
@@ -1,42 +1,19 @@
-let express = require('express');
-let app = express();
-
-/*
-Aunque un servidor HTML sirve HTML, una API sirve datos. 
-Una API REST (Transferencia de Estado Representacional) permite el intercambio de datos de manera simple, 
-sin que los clientes necesiten conocer ningún detalle sobre el servidor. 
-El cliente solo necesita saber dónde está el recurso (la URL) y la acción que desea realizar sobre él (el verbo). 
-El verbo GET se usa cuando estás obteniendo información sin modificar nada. Hoy en día, el formato de datos preferido para mover información en la web es JSON. 
-En pocas palabras, JSON es una forma conveniente de representar un objeto JavaScript como una cadena, 
-para que pueda transmitirse fácilmente.
-*/
+const express = require('express');
+const app = express();
+require('dotenv').config(); // Cargar variables de entorno desde .env
 
 app.get('/json', (req, res) => {
-    let message = "Hello json";
-    if (process.env.MESSAGE_STYLE === 'uppercase') {
-      message = message.toUpperCase();
-    }
-    res.json({ message });
-  });
-  
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
+    message = message.toUpperCase();
+  }
+  res.json({ message });
+});
 
-
-app.use('/public', express.static(__dirname + '/public'));
-
-app.get("/",
-    function(req, res)
-    {
-        var absolutePath = __dirname + '/views/index.html';
-        res.sendFile(absolutePath);
-    }
-);
-
-
-
-
-
-
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 
 
